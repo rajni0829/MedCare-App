674cfd50f056c360bd1781da89712449d5d5da3a
Saksham Agarwal <skshamagarwal@gmail.com>
2020-12-16 18:09:16 +0530

    icon

--- lib/con_main.dart
+++ lib/con_main.dart
@@ -22,66 +22,68 @@ class GetStarted extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-          child: Column(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(left: 20, right: 20),
+      body: SingleChildScrollView(
+        child: Container(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(height: 100),
-                Text(
-                  "Choose The Doctor\nYou Want",
-                  style: GoogleFonts.roboto(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "Tag Line",
-                  style: GoogleFonts.roboto(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 13,
-                    color: Colors.black54,
-                  ),
-                ),
-                SizedBox(height: 20),
-                RaisedButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16)),
-                  color: Colors.red[400],
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) => MyHomePage()));
-                  },
-                  child: Text(
-                    "Get Started",
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 100),
+                  Text(
+                    "Choose The Doctor\nYou Want",
                     style: GoogleFonts.roboto(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
                     ),
                   ),
-                )
-              ],
-            ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            margin: EdgeInsets.all(12),
-            height: 350,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/intro1.png"),
-                fit: BoxFit.cover,
+                  SizedBox(height: 10),
+                  Text(
+                    "Tag Line",
+                    style: GoogleFonts.roboto(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 13,
+                      color: Colors.black54,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16)),
+                    color: Colors.red[400],
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => MyHomePage()));
+                    },
+                    child: Text(
+                      "Get Started",
+                      style: GoogleFonts.roboto(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
-          )
-        ],
-      )),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.all(12),
+              height: 350,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/intro1.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            )
+          ],
+        )),
+      ),
     );
   }
 }
